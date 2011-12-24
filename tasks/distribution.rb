2e597d7ed40180e2f906bf9c6fa2c5a33386b6c2
Pat Allan <pat@freelancing-gods.com>
2011-12-24 16:33:16 +1100

    Remove git-ignored database.yml from gemspec.

--- tasks/distribution.rb
+++ tasks/distribution.rb
@@ -10,20 +10,22 @@ Jeweler::Tasks.new do |gem|
   gem.email       = "pat@freelancing-gods.com"
   gem.homepage    = "http://github.com/freelancing-god/ts-datetime-delta"
   gem.authors     = ["Pat Allan"]
-  
+
   gem.add_dependency 'thinking-sphinx', '>= 1.3.8'
-  
+
   gem.add_development_dependency "rspec",    ">= 1.2.9"
   gem.add_development_dependency "yard",     ">= 0"
   gem.add_development_dependency "cucumber", ">= 0"
-  
+
   gem.files = FileList[
     'lib/**/*.rb',
     'LICENSE',
     'README.textile'
   ]
   gem.test_files = FileList[
-    'features/**/*',
+    'features/**/*.rb',
+    'features/**/*.feature',
+    'features/support/database.example.yml',
     'spec/**/*'
   ]
 end
